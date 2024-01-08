977d8b589df96e875fea09e5fd9cc4b75bd3b964
s83582003 <31023720-s83582003@users.noreply.replit.com>
2024-01-08 08:05:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+
+from keep_alive import keep_alive
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print("The bot is ready, and online!")
+
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+  if msg.content.startswith("$help"):
+    await msg.channel.send("may I help you?")
+
+try:
+  keep_alive()
+  client.run(os.environ['TOKEN'])
+except Exception as err:
+  raise err
